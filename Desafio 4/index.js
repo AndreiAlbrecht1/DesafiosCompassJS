2d71973ceaa4393abdce6dc2fd843fdb922e6eb7
Andrei Albrecht <andreialbrecht@gmail.com>
2025-03-14 18:11:48 -0300

    Primeira parte pronta

--- Desafio 4/index.js
+++ Desafio 4/index.js
@@ -1,3 +1,26 @@
 const input = "pedra"
 const opcoes = ["pedra", "papel", "tesoura"]
 const escolhaCpu = opcoes[Math.floor(Math.random() * 3)]
+console.log(escolhaCpu);
+
+
+switch (input) {
+    case escolhaCpu:
+        avisar("empatou")
+        break;
+    case opcoes[0]:
+        escolhaCpu == opcoes[1] ? avisar("perdeu") : avisar("ganhou")
+        break;
+    case opcoes[1]:
+        escolhaCpu == opcoes[2] ? avisar("perdeu") : avisar("ganhou")
+        break;
+    case opcoes[2]:
+        escolhaCpu == opcoes[0] ? avisar("perdeu") : avisar("ganhou")
+        break;
+    default:
+        console.log("Input inválido");    
+}
+
+function avisar(texto) {
+    console.log("Você " + texto + "!");
+}
