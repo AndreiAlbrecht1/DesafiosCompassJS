a760d85d3172301b4e27f20362b7e432103f67e6
Andrei Albrecht <andreialbrecht@gmail.com>
2025-03-18 13:42:46 -0300

    refactor(cobrinha): refatoracao código (HTML, CSS, JS) e adicao de funcionalidades (score, record, botões de início/reinício, teclas WASD)

--- Cobrinha/index.js
+++ Cobrinha/index.js
@@ -1,40 +1,84 @@
-let cobra = [{x: 10, y: 10},{x: 11, y: 10}]
+let cobra = [{x: 10, y: 10},{x: 11, y: 10}];
 let posicaoAtual = cobra[0]
 let direcaoAtual = "direita"
-let tecla = "ArrowRight"
+let direcaoTecla = "cima"
 let jogoAtivo
-const movimentacao = {
-    cima: 'ArrowDown',
-    baixo: 'ArrowUp',
-    esquerda: 'ArrowLeft',
-    direita: 'ArrowRight'
+const movimentos = ['cima', 'baixo', 'esquerda', 'direita']
+const teclasMovimentos = {
+    'w': 'cima',
+    'ArrowUp': 'cima',
+    's': 'baixo',
+    'ArrowDown': 'baixo',
+    'a': 'esquerda',
+    'ArrowLeft': 'esquerda',
+    'd': 'direita',
+    'ArrowRight': 'direita'
 };
 const mapa = document.getElementById('mapa')
 const colunas = 20;
 const linhas = 20;
-
+let score = 0;
+let record = 0;
+let velocidade = 200;
 
 criarMapa()
+document.getElementById('gameRestart').style.display = 'none';
+document.getElementById("startButton").addEventListener("click", startGame);
+document.getElementById("restartButton").addEventListener("click", restartGame);
 
 document.addEventListener('keydown', (event) => {
-    if (Object.values(movimentacao).includes(event.key)) {
-        tecla = event.key;
+    if (teclasMovimentos[event.key]) {
+        direcaoTecla = teclasMovimentos[event.key]
         event.preventDefault();
     }
 });
 
 function startGame() {
+    document.getElementById('gameStart').style.display = 'none';
     gerarComida()
-    jogoAtivo = setInterval(movimentar, 250)
+    jogoAtivo = setInterval(movimentar, velocidade)
+}
+
+function restartGame() {
+    limparMapa()
+    score = 0
+    atualizarScore()
+    velocidade = 200;
+    cobra = [{x: 10, y: 10}, {x: 11, y: 10}]
+    posicaoAtual = cobra[0]
+    direcaoAtual = "direita"
+    direcaoTecla = "cima"
+    document.getElementById('gameRestart').style.display = 'none';
+    startGame();
 }
 
 function gameOver() {
     clearInterval(jogoAtivo)
+    if (score > record) {
+        record = score
+        document.getElementById('scoreRecord').textContent = `Record: ${record}`;
+    }
+    document.getElementById('gameRestart').style.display = 'flex';
+    document.getElementById('scoreFinal').textContent = `Seu Score Final: ${score}`
+}
+
+function limparMapa() {
+    let frutas = document.querySelectorAll('.fruta__mapa')
+    frutas.forEach(fruta => {
+        fruta.classList.remove('fruta__mapa')
+        fruta.classList.add('casa__mapa')
+    });
+
+    let partesCobra = document.querySelectorAll('.cobra__mapa'); 
+    partesCobra.forEach(parte => {
+        parte.classList.remove('cobra__mapa')
+        parte.classList.add('casa__mapa')
+    });
 }
 
 function gerarComida() {
-    let x = Math.floor(Math.random() * (10 - 1) + 1)
-    let y = Math.floor(Math.random() * (10 - 1) + 1)
+    let x = Math.floor(Math.random() * (20 - 1) + 1)
+    let y = Math.floor(Math.random() * (20 - 1) + 1)
     let z = {x:x, y:y}
 
     for (let i = 0; i < cobra.length; i++) {
@@ -57,22 +101,34 @@ function comerComida() {
     aux2.classList.remove('fruta__mapa')
     aux2.classList.add('casa__mapa')
     gerarComida()
+    score += 1
+    atualizarScore()
+    aumentarVelocidade()
+}
+
+function atualizarScore() {
+    document.getElementById('scoreAtual').textContent = `Score atual: ${score}`;
+}
+
+function aumentarVelocidade() {
+        velocidade *= 0.9
+        clearInterval(jogoAtivo)
+        jogoAtivo = setInterval(movimentar, velocidade)
 }
 
 function movimentar() {
     posicaoAtual = cobra[0]
-    
-    switch (tecla) {
-        case movimentacao.cima:
+    switch (direcaoTecla) {
+        case movimentos[0]:
             upper()
             break;
-        case movimentacao.baixo:
+        case movimentos[1]:
             down()
             break;
-        case movimentacao.esquerda:
+        case movimentos[2]:
             left()
             break;
-        case movimentacao.direita:
+        case movimentos[3]:
             right()
             break;
         default:
@@ -80,24 +136,26 @@ function movimentar() {
     }
     JSON.stringify(cobra[0]) == JSON.stringify(comida) ? comerComida() : cortarRabo()
     testarPosicao()
-    mostrarCobra()
 }
 
 function testarPosicao() {
     if (cobra[0].x < 1 || cobra[0].x > colunas || cobra[0].y < 1 || cobra[0].y > linhas) {
         gameOver()
+        return
     }
     for (let i = 1; i < cobra.length; i++) {
         if (JSON.stringify(cobra[i]) == JSON.stringify(cobra[0])){
             gameOver()
+            return
         } 
     }
+    mostrarCobra()
 }
 
 function upper() {
     if (direcaoAtual !== "baixo") {
         const xx = (posicaoAtual.x)
-        const yy = (posicaoAtual.y) + 1
+        const yy = (posicaoAtual.y) - 1
         cobra.unshift({x: xx, y: yy})
         direcaoAtual = "cima"
     } else{
@@ -107,7 +165,7 @@ function upper() {
 function down() {  
     if (direcaoAtual !== "cima") {
         const xx = (posicaoAtual.x)
-        const yy = (posicaoAtual.y) - 1
+        const yy = (posicaoAtual.y) + 1
         cobra.unshift({x: xx, y: yy})
         direcaoAtual = "baixo"
     } else{
