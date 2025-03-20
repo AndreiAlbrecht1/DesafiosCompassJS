8db1890341552d262c387273d390dd99ab52bc14
Andrei Albrecht <andreialbrecht@gmail.com>
2025-03-20 20:39:51 -0300

    feat(api): adicionado endpoint  para análise do preço do Bitcoin

--- ServerHTTP/server.js
+++ ServerHTTP/server.js
@@ -14,7 +14,6 @@ const server = createServer((req, res) => {
         const url = new URL(req.url, `http://${hostname}:${port}`);
         const pathParts = url.pathname.split('/').filter(Boolean);
         
-
         if (req.method === 'GET' && url.pathname === '/health-check') {
 
             res.statusCode = 200;
@@ -75,6 +74,65 @@ const server = createServer((req, res) => {
                     res.end(JSON.stringify({ error: "Invalid JSON body" }));
                 }
             });
+        } else if(req.method === 'GET' && (url.pathname === '/stock-insight' || pathParts[0] === 'stock-insight')){
+            
+            let currency = ''
+            const messagesOptions = ['Bom momento para compra!', 'Preço razoável. Avalie antes de comprar.', 'Bitcoin está caro. Pode ser melhor esperar.']
+            let message = ''
+
+            if (pathParts.length > 1) {
+                currency = pathParts[1]
+            } else {
+                currency = (Object.fromEntries(url.searchParams.entries())).currency
+            }
+
+            function calcularInsight(usd,brl) {
+                
+                if (Boolean(currency) === false || currency === 'usd') {
+                    currency = 'usd'
+                    if (usd < 60000) {
+                        message = messagesOptions[0]
+                    } else if (usd > 80000) {
+                        message = messagesOptions[2]
+                    } else {
+                        message = messagesOptions[1]
+                    }
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({btc_price: usd, currency: currency, suggestion: message}));
+                } else if (currency === 'brl') {
+                    if (brl < 300000) {
+                        message = messagesOptions[0]
+                    } else if (brl > 450000) {
+                        message = messagesOptions[2]
+                    } else {
+                        message = messagesOptions[1]
+                    }
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({btc_price: brl, currency: currency, suggestion: message}));
+                } else{
+                    res.statusCode = 400;
+                    res.end(JSON.stringify({ error: "Invalid input" }));
+                }
+            }
+
+            
+
+            fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd%2Cbrl&precision=2')
+                .then(resFetch => resFetch.json())
+                .then(dataFetch => {
+                    if (dataFetch?.status?.error_code === 429) {
+                        res.statusCode = 400;
+                        res.end(JSON.stringify({ error: "Excesso de Requisições Feitas!!! Aguarde um instante" }))
+                    }
+                    const usd = dataFetch.bitcoin.usd
+                    const brl = dataFetch.bitcoin.brl
+                    calcularInsight(usd,brl)
+                })
+                .catch(error => {
+                    console.error(error)
+                    res.statusCode = 500;
+                    res.end(JSON.stringify({ error: 'Internal Server Error' }))
+                });
         } else {
 
             res.statusCode = 404;
